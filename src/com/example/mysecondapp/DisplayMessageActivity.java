940e163a1bd4f958dec930468645a017468c3fed
Marco Lau <lau.career@gmail.com>
2013-08-23 22:15:58 +0800

    Receive the intent and display the message. Wonder why the Android developer guide skips 'Adding the Action Bar' and continues at 'Managing the Activity Lifecycle'

--- src/com/example/mysecondapp/DisplayMessageActivity.java
+++ src/com/example/mysecondapp/DisplayMessageActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 
 public class DisplayMessageActivity extends Activity {
@@ -15,7 +17,17 @@ public class DisplayMessageActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display_message);
+		
+		// Get the message from the intent
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		// Create the text view
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+		
+		setContentView(textView);
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
